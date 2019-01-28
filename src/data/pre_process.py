740a91ee37340be3deef455ece767d9e24c6f2bf
Neeraj Lakkapragada <lneeraj97@gmail.com>
2019-01-28 15:56:59 +0530

    Data Augmentation done

--- src/data/pre_process.py
+++ src/data/pre_process.py
@@ -3,40 +3,32 @@ import os
 import sys
 import numpy as np
 
-SOURCE_PATH = './data/interim'
-DEST_PATH = './data/processed'
+SOURCE_PATH = './data/raw/Base11'
+DEST_PATH = './data/interim'
 
 
-def preprocess_image(curr_path, dest_path):
-    image = cv.imread(curr_path, 0)
-    """ cv.imshow('image', image)
-    cv.waitKey(100) """
+def preprocess_image(image_path):
+    image = cv.imread(image_path, 0)
     image = cv.resize(image, (256, 256))
+    cv.imshow('image', image)
+    cv.waitKey(100)
 
 
-def process_folder(curr_folder):
-    # Get path of the destination folder by calculating the relative path
-    dest_folder = os.path.join(
-        DEST_PATH, os.path.relpath(curr_folder, SOURCE_PATH))
-
+def process_folder(folder):
     # Get current and destination paths of every image and call the pre_processing function on it
-    for image in os.listdir(curr_folder):
+    for image in os.listdir(folder):
         # Full source path of the image
-        curr_path = os.path.join(curr_folder, image)
-
-        # Full destination path of the image
-        dest_path = os.path.join(dest_folder, image)
-
-        # print(curr_path, dest_path)
-        preprocess_image(curr_path, dest_path)
+        image_path = os.path.join(folder, image)
+        preprocess_image(image_path)
+        # preprocess_image(image_path)
         # exit()
 
 
-def start_preprocessing():
+# def start_preprocessing():
     # Call the process_folder function on each folder
-    for folder in os.listdir(SOURCE_PATH):
-        process_folder(os.path.join(SOURCE_PATH, folder))
-        # print(os.path.join(SOURCE_PATH, folder))
+    # for folder in os.listdir(SOURCE_PATH):
+    #   process_folder(os.path.join(SOURCE_PATH, folder))
+    # start_preprocessing()
 
 
-start_preprocessing()
+process_folder(SOURCE_PATH)
