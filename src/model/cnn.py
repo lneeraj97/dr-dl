8aa86c7e601c706ca693ad10a1af415c41d368c5
Neeraj Lakkapragada <lneeraj97@gmail.com>
2019-02-08 17:00:01 +0530

    Modified loss functions and class mode

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -1,15 +1,15 @@
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-
+from keras.callbacks import ModelCheckpoint
 
 TRAIN = 'data/processed/train'
 TEST = 'data/processed/test'
 JSON_FILE = './model/model.json'
-HDF5_FILE = './model/model.h5'
-EPOCHS = 30
-BATCH_SIZE = 32
-STEPS_PER_EPOCH = 130
+HDF5_FILE = './model/weights.h5'
+EPOCHS = 1000
+BATCH_SIZE = 64
+STEPS_PER_EPOCH = 65
 VALIDATION_STEPS = 22
 POOL_SIZE = (2, 2)
 KERNEL_SIZE = (3, 3)
@@ -55,27 +55,34 @@ def create_model():
     # FC layers
     model.add(Dense(units=2048, activation='relu'))
     model.add(Dense(units=512, activation='relu'))
-    model.add(Dense(units=4, activation='sigmoid'))
+    model.add(Dense(units=4, activation='softmax'))
 
     return model
 
 
 def init_model(model):
-    model.compile(optimizer='adam', loss='binary_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['categorical_accuracy'])
     return model
 
 
 def train_model(model):
+    filepath = HDF5_FILE
+    checkpoint = ModelCheckpoint(
+        filepath, monitor='val_categorical_accuracy', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
+
     train_datagen = ImageDataGenerator(
         rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-    test_datagen = ImageDataGenerator(rescale=1./255)
     train_set = train_datagen.flow_from_directory(
         TRAIN, target_size=TARGET_SIZE, batch_size=BATCH_SIZE, class_mode='categorical')
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
     test_set = test_datagen.flow_from_directory(
         TEST, target_size=TARGET_SIZE, batch_size=BATCH_SIZE, class_mode='categorical')
+
     model.fit_generator(train_set, steps_per_epoch=STEPS_PER_EPOCH,
-                        epochs=EPOCHS, validation_steps=VALIDATION_STEPS, validation_data=test_set)
+                        epochs=EPOCHS, validation_steps=VALIDATION_STEPS, validation_data=test_set, callbacks=callbacks_list)
 
     return model
 
